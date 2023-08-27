8daa10ba288b82b5d5592a05c5f06bf9fc46d3e2
patrick <paddyyates04@gmail.com>
2023-08-27 13:54:22 +0100

    problem 1
    find the sum of all of the multiples of 3 and 5 below 1000

---
+++ problem1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int findMult3Or5(){
+	int i = 0;
+	int total = 0;
+
+	for (i = 0; i < 1000; i++) {
+		if ((i % 3) == 0) {
+			total += i;
+		}
+		else if ((i % 5) == 0){
+			total += i;
+		}
+	}
+
+	return total;
+}
+
+
+int main(){
+	int total = findMult3Or5();
+
+	printf("%d", total);
+}
